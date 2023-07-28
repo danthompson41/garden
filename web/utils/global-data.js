a906d9eda6c0ffe79bc0b589b151a5c66693bade
Dan Thompson <danthompson41@gmail.com>
2023-07-28 16:22:47 -0400

    Fixing git ignore

--- web/utils/global-data.js
+++ web/utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+  const name = process.env.APP_NAME
+    ? decodeURI(process.env.APP_NAME)
+    : 'The Garden';
+  const blogTitle = process.env.APP_TITLE
+    ? decodeURI(process.env.APP_TITLE)
+    : 'Habit tracker';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
